8144e308fb8075d61f25f8ec78ac61ca9d960659
defaultUser <71094683+Pablo-pelaez@users.noreply.github.com>
2021-10-20 09:30:18 -0500

    se actualiza

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8081/casaMiercoles';
+    public $baseURL = 'http://localhost:8081/casahogar';
 
     /**
      * --------------------------------------------------------------------------
